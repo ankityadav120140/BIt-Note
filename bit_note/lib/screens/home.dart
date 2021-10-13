cef8fa8a6259dd28672a7986fe56995114bfb6fd
ankityadav120140 <ankityadav120140@gmail.com>
2021-10-13 14:25:37 +0530

    centered title

--- bit_note/lib/screens/home.dart
+++ bit_note/lib/screens/home.dart
@@ -56,44 +56,67 @@ class _HomeState extends State<Home> {
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    margin: EdgeInsets.only(top: 5, left: 45, right: 65),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.purple.shade900,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "TO-DO List",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //   children: [
+              //     Container(
+              //       padding: EdgeInsets.all(5),
+              //       margin: EdgeInsets.only(top: 5, left: 45, right: 65),
+              //       decoration: BoxDecoration(
+              //         borderRadius: BorderRadius.circular(10),
+              //         color: Colors.purple.shade900,
+              //       ),
+              //       child: Center(
+              //         child: Text(
+              //           "TO-DO List",
+              //           style: TextStyle(
+              //             fontSize: 20,
+              //             color: Colors.white,
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //     Container(
+              //       padding: EdgeInsets.all(5),
+              //       margin: EdgeInsets.only(top: 5, right: 5),
+              //       decoration: BoxDecoration(
+              //         borderRadius: BorderRadius.circular(10),
+              //         color: Colors.amber,
+              //       ),
+              //       child: Center(
+              //         child: Text(
+              //           "Scheduled For Today",
+              //           style: TextStyle(
+              //             fontSize: 20,
+              //             color: Colors.white,
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 5, left: 5),
+                        child: Center(
+                          child: Text("DATA1"),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(5),
-                    margin: EdgeInsets.only(top: 5, right: 5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.amber,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Scheduled For Today",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(right: 5, left: 5),
+                        child: Center(
+                          child: Text("DATA2"),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
               Expanded(
                 flex: 7,
@@ -108,16 +131,9 @@ class _HomeState extends State<Home> {
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.purpleAccent.withOpacity(0.5),
                         ),
-                        child: Container(
-                          padding: EdgeInsets.all(5),
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: [
-                                Text(
-                                    "Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus.Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus.Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus."),
-                              ],
-                            ),
-                          ),
+                        child: SingleChildScrollView(
+                          child: Text(
+                              "Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus.Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus.Vitae fringilla. Arcu a commodo class massa potenti magna lacinia malesuada tortor metus cursus. Cum accumsan tempus mattis et potenti quis sociis turpis vestibulum egestas vitae. Ultricies ridiculus turpis lorem aenean neque erat, vitae nascetur.Porttitor auctor ipsum. Orci sodales volutpat sodales magnis inceptos nec torquent vehicula curabitur habitant platea facilisis placerat. Dis non laoreet.Dignissim. Metus conubia class interdum fusce convallis cras. Dictumst faucibus bibendum. Platea mus Nibh rhoncus. Per in posuere, tristique iaculis sed etiam neque scelerisque Sapien conubia quam sed dictumst bibendum non rutrum quisque integer, habitasse hendrerit nec et mus magnis mattis inceptos ante quam lectus."),
                         ),
                       ),
                     ),
